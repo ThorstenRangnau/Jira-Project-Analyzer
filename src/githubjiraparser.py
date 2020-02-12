354d0b512ea092107c1abeba1a75ec5127efd7ca
Thorsten Rangnau <rangnau@adference.com>
2020-02-12 17:24:59 +0100

    Establish connection to apache jira server in order to receive information for specific apache project

--- src/githubjiraparser.py
+++ src/githubjiraparser.py
@@ -1,5 +1,6 @@
 import argparse
 from github import Github
+from jira import JIRA
 
 
 def parse_args():
@@ -16,10 +17,37 @@ def parse_args():
 def parse_github(repository_title):
     github = Github()
     repository = github.get_repo(repository_title)
+    open_pull_requests = repository.get_pulls()
+    for pr in open_pull_requests:
+        print(pr.title)
+        print(pr.state)
+        commits = pr.get_commits()
+        for c in commits:
+            print(c.commit.message)
+    # closed_pull_requests = repository.get_pulls()
 
 '''
 python githubjiraparser.py -gr apache/activemq in order to parse apache active mq
+
+TODOs:
+
+1. Receive all issues from jira for project
+
+2. Parse all issues for issue indicator: Issue title: Prefix - number - description
+
+3. Use these information to filter all pr and commit title/messages for prefix and number
+
+4. map jira issues with git prs and commits
 '''
+
+
+def parse_jira(project_name):
+    jira = JIRA('https://issues.apache.org/jira/')
+    project = jira.project(project_name)
+    print(project)
+
+
 if __name__ == "__main__":
     args = parse_args()
-    parse_github(args.gitrepo)
+    parse_jira(args.jiraproject)
+    # parse_github(args.gitrepo)

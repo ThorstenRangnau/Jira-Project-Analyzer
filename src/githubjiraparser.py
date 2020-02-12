a171991ecd52ae30fd0d5d0cd10c326f79bf73e6
Thorsten Rangnau <rangnau@adference.com>
2020-02-12 19:38:04 +0100

    Check if jira issue prefix is used in git pull request title

--- src/githubjiraparser.py
+++ src/githubjiraparser.py
@@ -1,6 +1,23 @@
 import argparse
+import re
 from github import Github
-from jira import JIRA
+from jira import JIRA, JIRAError
+
+'''
+python githubjiraparser.py -gr apache/activemq in order to parse apache active mq
+
+python githubjiraparser.py -jp Cassandra -gr apache/cassandra
+
+TODOs:
+
+1. Receive all issues from jira for project:
+
+2. Parse all issues for issue indicator: Issue title: Prefix - number - description
+
+3. Use these information to filter all pr and commit title/messages for prefix and number
+
+4. map jira issues with git prs and commits
+'''
 
 
 def parse_args():
@@ -14,40 +31,44 @@ def parse_args():
     return parser.parse_args()
 
 
-def parse_github(repository_title):
+def parse_github(repository_title, prefix):
     github = Github()
     repository = github.get_repo(repository_title)
-    open_pull_requests = repository.get_pulls()
-    for pr in open_pull_requests:
-        print(pr.title)
-        print(pr.state)
-        commits = pr.get_commits()
-        for c in commits:
-            print(c.commit.message)
-    # closed_pull_requests = repository.get_pulls()
-
-'''
-python githubjiraparser.py -gr apache/activemq in order to parse apache active mq
-
-TODOs:
-
-1. Receive all issues from jira for project
-
-2. Parse all issues for issue indicator: Issue title: Prefix - number - description
-
-3. Use these information to filter all pr and commit title/messages for prefix and number
-
-4. map jira issues with git prs and commits
-'''
+    pull_requests = repository.get_pulls()
+    for pr in pull_requests:
+        if prefix in pr.title:
+            # note this is for the moment in order to increase performance
+            return True
+        # print(pr.title)
+        # print(pr.state)
+        # commits = pr.get_commits()
+        # TODO: atm we require log in to parse commits because of rate limit of 60
+        # TODO: check how many prs/commits have prefix included
+        # TODO: store results in order to map issue num and pr/commit
+    return False
 
 
 def parse_jira(project_name):
     jira = JIRA('https://issues.apache.org/jira/')
-    project = jira.project(project_name)
-    print(project)
+    projects = jira.projects()
+    project = None
+    for p in projects:
+        if p.name == project_name:
+            project = p
+    if project is None:
+        print("No such project %s" % project_name)
+        return
+    issues = jira.search_issues("project = %s" % project.name, maxResults=1)
+    for issue in issues:
+        issue_name = issue.key
+    issue_prefix = re.sub("\d+", "", issue_name)
+    issue_prefix = re.sub("-", "", issue_prefix)
+    return issue_prefix
 
 
 if __name__ == "__main__":
     args = parse_args()
-    parse_jira(args.jiraproject)
-    # parse_github(args.gitrepo)
+    jira_issue_prefix = parse_jira(args.jiraproject)
+    print("Issue prefix is %s" % jira_issue_prefix)
+    repo_has_prefix = parse_github(args.gitrepo, jira_issue_prefix)
+    print("%s" % "Repo has prefix!" if repo_has_prefix else "Repo has not prefix!")

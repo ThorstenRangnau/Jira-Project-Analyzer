bb891cb8ecdf7c3301d9ec79dcd04e02e11708c3
Thorsten Rangnau <rangnau@adference.com>
2020-03-29 23:46:57 +0200

    Order dictionary for smells by commit id

--- src/smellaggregator.py
+++ src/smellaggregator.py
@@ -18,31 +18,42 @@ def parse_args():
     parser.add_argument(
         "-o", dest="output_directory", required=True,
         help="Path to output directory -- the location where the output file will be placed!")
+    parser.add_argument(
+        "-p", dest="only_package", required=False, default=False, action="store_true",
+        help="Considers only package level architectural smells!")
     return parser.parse_args()
 
 
 class ArchitecturalSmell(object):
 
-    def __init__(self, unique_smell_id, birth_version):
+    def __init__(self, unique_smell_id, birth_version, affected_components):
         self.unique_smell_id = unique_smell_id
         self.birth_version = birth_version
+        self.affected_components = affected_components
 
 
-def read_architectural_smells(input_file):
+def extract_architectural_smell(row):
+    return ArchitecturalSmell(row["uniqueSmellID"], row["firstAppeared"], row["affectedElements"])
+
+
+def read_architectural_smells(input_file, only_package):
     architectural_smells = dict()
     with open(input_file, mode="r") as csv_file:
         for row in csv.DictReader(csv_file):
-            unique_smell_id = row["uniqueSmellID"]
-            if unique_smell_id not in architectural_smells:
-                birth_version = row["firstAppeared"]
-                # does not exist yet and hence need to be created!
-                smell = ArchitecturalSmell(unique_smell_id, birth_version)
-                architectural_smells[unique_smell_id] = smell
+            # in order to minimize github api usage we store smells by birth version
+            # commit_id: [smell1, smell2, ...]
+            if only_package and row["affectedComponentType"] == "class":
+                continue
+            commit_id = row["firstAppeared"]
+            if commit_id not in architectural_smells:
+                architectural_smells[commit_id] = [extract_architectural_smell(row)]
+            else:
+                architectural_smells[commit_id].append(extract_architectural_smell(row))
     return architectural_smells
 
 
 if __name__ == "__main__":
     args = parse_args()
-    smells = read_architectural_smells(args.input_file)
+    smells = read_architectural_smells(args.input_file, args.only_package)
     print("We extracted %d smells" % len(smells))
     print(args.output_directory)

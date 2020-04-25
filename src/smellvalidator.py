f75041922cce314a35d3298a881410ac3ff249db
Thorsten Rangnau <rangnau@adference.com>
2020-04-25 22:58:17 +0200

    Add import of Designite smells

--- src/smellvalidator.py
+++ src/smellvalidator.py
@@ -1,6 +1,6 @@
 import argparse
 
-from validation.importer import ASTrackerImporter
+from validation.importer import ASTrackerImporter, DesigniteImporter
 
 
 def parse_args():
@@ -11,21 +11,20 @@ def parse_args():
     parser.add_argument(
         "-c", dest="commit_sha", required=True,
         help="Commit_sha that needs validation")
-    # parser.add_argument(
-    #     "-eS", dest="path_to_existing_smells", required=True,
-    #     help="Path to input file that contains the existing smells")
+    parser.add_argument(
+        "-eS", dest="path_to_existing_smells", required=True,
+        help="Path to input file that contains the existing smells")
     return parser.parse_args()
-
-
-def import_astracker_smells(path_to_validation_smells, issue_key):
-    return ASTrackerImporter().import_analysis_results(path_to_validation_smells, issue_key)
 
 
 if __name__ == '__main__':
     args = parse_args()
-    astracker_results = import_astracker_smells(args.path_to_validation_smells, args.commit_sha)
+    astracker_results = ASTrackerImporter().import_analysis_results(args.path_to_validation_smells, args.commit_sha)
+    designite_results = DesigniteImporter().import_analysis_results(args.path_to_existing_smells)  # no need for commit sha here
     print(astracker_results)
     print(len(astracker_results))
+    print(designite_results)
+    print(len(designite_results))
 
 
-# python smellvalidator.py -vS /Users/trangnau/RUG/master-thesis/results/tajo/architectural_smells_tajo.csv -c 0000793a57b4504b3454adfa8ff7e02262e047a0
+# python smellvalidator.py -vS /Users/trangnau/RUG/master-thesis/results/tajo/architectural_smells_tajo.csv -eS /Users/trangnau/RUG/master-thesis/designite/results/tajo/ArchitectureSmells-843.csv -c 0000793a57b4504b3454adfa8ff7e02262e047a0
--- src/validation/importer.py
+++ src/validation/importer.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 from validation.smell import CyclicDependency
 
@@ -18,18 +19,35 @@ class FileImporter(object):
                 # checking for issue is merely an optimization for astracker results
                 if commit_sha is not None and row["commit_sha"] != commit_sha:
                     continue
-                smells[idx] = self.convert_csv_row(row)
+                smells[idx] = self.convert_csv_row(row, idx)
         return smells
 
-    def convert_csv_row(self, csv_file):
+    def convert_csv_row(self, row, smell_id):
         raise Exception("Method not implemented")
 
 
 class ASTrackerImporter(FileImporter):
 
-    def convert_csv_row(self, row):
+    def convert_csv_row(self, row, smell_id):
         smell_type = row["smell_type"]
         smell = None
         if smell_type == "cyclicDep":
             smell = CyclicDependency(row["unique_smell_id"], row["affected_elements"])
+        return smell
+
+
+def extract_cyclic_components(smell_cause):
+    smell_list = re.findall("(?:^\w+|\w+\.\w+)+", smell_cause)
+    if "The" in smell_list:
+        smell_list.remove("The")
+    return smell_list
+
+
+class DesigniteImporter(FileImporter):
+
+    def convert_csv_row(self, row, smell_id):
+        smell_type = row["Architecture Smell"]
+        smell = None
+        if smell_type == "Cyclic Dependency":
+            smell = CyclicDependency(smell_id, extract_cyclic_components(row["Cause of the Smell"]))
         return smell

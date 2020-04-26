3aa99a40626ed0878c935a85b1580818965a47b4
Thorsten Rangnau <rangnau@adference.com>
2020-04-26 12:05:30 +0200

    Establish first validation of analyzed semlls

--- src/validation/smell_validator.py
+++ src/validation/smell_validator.py
@@ -1,12 +1,24 @@
+import operator
+
+
 class SmellValidator(object):
 
     def validate_smell(self, validation_smells, existing_smells):
-        # check validation smell
+        # check cyclic dependencies
         for smell in validation_smells.cyclic_dependencies:
-            matches = 0
+            match_list = dict()
             for existing_smell in existing_smells.cyclic_dependencies:
+                matches = 0
                 for component in smell.participating_components:
+                    match = False
                     for existing_component in existing_smell.participating_components:
                         if component.name == existing_component.name:
-                            matches += 1
-            print("Found %d matches for smell with id %s" % (matches, smell.id))
+                            match = True
+                    if match:
+                        matches += 1
+                if matches >= 2:
+                    match_list[existing_smell.id] = matches
+            print("Found %d similar cycles for smell with id %s" % (len(match_list), smell.id))
+            key = max(match_list.items(), key=operator.itemgetter(1))[0]
+            print("The most similar cycle is id %s with %d matched components" % (key, match_list[key]))
+

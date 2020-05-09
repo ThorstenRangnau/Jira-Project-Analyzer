1903cacf0a3c0fa507f754d64073edb871ad9a08
Thorsten Rangnau <rangnau@adference.com>
2020-05-09 15:58:15 +0200

    reconfigure astracker directroy studio batch script

--- scripts/data_store/astracker/astracker_directory_studio.sh
+++ scripts/data_store/astracker/astracker_directory_studio.sh
@@ -1,41 +1,30 @@
 #!/bin/bash
-#SBATCH --time=10-00:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
 #SBATCH --job-name=astracker_directory_studio
-#SBATCH --mem=120GB
+#SBATCH --mem=64GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=t.rangnau@student.rug.nl
 #SBATCH --output=job-%j-astracker-directory_studio.log
 #SBATCH --partition=regular
+
 
 echo Load modules ...
 ml load OpenJDK/11.0.2
 
 echo Create folder ...
 
-mkdir $TMPDIR/archive-tmp
-mkdir $TMPDIR/classes
-mkdir $TMPDIR/generated-sources
-mkdir $TMPDIR/maven-status
 mkdir $TMPDIR/directory_studio_arcan_analysis
 
 echo Copy files ...
 
-cp /home/s3570282/ondemand/data/astracker/target/astracker-0.9.0-jar-with-dependencies.jar $TMPDIR
-cp -rf /home/s3570282/ondemand/data/astracker/target/archive-tmp/ $TMPDIR/archive-tmp/
-cp -rf /home/s3570282/ondemand/data/astracker/target/classes/ $TMPDIR/classes/
-cp -rf /home/s3570282/ondemand/data/astracker/target/generated-sources/ $TMPDIR/generated-sources/
-cp -rf /home/s3570282/ondemand/data/astracker/target/maven-status/ $TMPDIR/maven-status/
+cp /home/s3570282/ondemand/data/astracker_v1/astracker-1.0.0-jar-with-dependencies.jar $TMPDIR
 cp -Rf /data/s3570282/results/directory_studio_arcan_analysis/. $TMPDIR/directory_studio_arcan_analysis/
 
 echo Change rights ...
 
-chmod +rwx $TMPDIR/astracker-0.9.0-jar-with-dependencies.jar
-chmod +rwx $TMPDIR/archive-tmp/
-chmod +rwx $TMPDIR/classes/
-chmod +rwx $TMPDIR/generated-sources/
-chmod +rwx $TMPDIR/maven-status/
+chmod +rwx $TMPDIR/astracker-1.0.0-jar-with-dependencies.jar
 chmod +rwx $TMPDIR/directory_studio_arcan_analysis/
 
 echo Check size of arcan_analysis
@@ -46,4 +35,4 @@ ls -l $TMPDIR/directory_studio_arcan_analysis/ | wc -l
 
 echo Start program ...
 
-java -Xmx96g -jar $TMPDIR/astracker-0.9.0-jar-with-dependencies.jar -i $TMPDIR/directory_studio_arcan_analysis/ -p directory_studio -o /data/s3570282/results/directory_studio_astracker_analysis/ -pC
+java -Xmx60g -jar $TMPDIR/astracker-1.0.0-jar-with-dependencies.jar -i $TMPDIR/directory_studio_arcan_analysis/ -p directory_studio -o /data/s3570282/results/directory_studio_astracker_analysis/ -pC
---
+++ scripts/data_store/astracker/astracker_directory_studio_old.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --time=10-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --job-name=astracker_directory_studio
+#SBATCH --mem=120GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=t.rangnau@student.rug.nl
+#SBATCH --output=job-%j-astracker-directory_studio.log
+#SBATCH --partition=regular
+
+echo Load modules ...
+ml load OpenJDK/11.0.2
+
+echo Create folder ...
+
+mkdir $TMPDIR/archive-tmp
+mkdir $TMPDIR/classes
+mkdir $TMPDIR/generated-sources
+mkdir $TMPDIR/maven-status
+mkdir $TMPDIR/directory_studio_arcan_analysis
+
+echo Copy files ...
+
+cp /home/s3570282/ondemand/data/astracker/target/astracker-0.9.0-jar-with-dependencies.jar $TMPDIR
+cp -rf /home/s3570282/ondemand/data/astracker/target/archive-tmp/ $TMPDIR/archive-tmp/
+cp -rf /home/s3570282/ondemand/data/astracker/target/classes/ $TMPDIR/classes/
+cp -rf /home/s3570282/ondemand/data/astracker/target/generated-sources/ $TMPDIR/generated-sources/
+cp -rf /home/s3570282/ondemand/data/astracker/target/maven-status/ $TMPDIR/maven-status/
+cp -Rf /data/s3570282/results/directory_studio_arcan_analysis/. $TMPDIR/directory_studio_arcan_analysis/
+
+echo Change rights ...
+
+chmod +rwx $TMPDIR/astracker-0.9.0-jar-with-dependencies.jar
+chmod +rwx $TMPDIR/archive-tmp/
+chmod +rwx $TMPDIR/classes/
+chmod +rwx $TMPDIR/generated-sources/
+chmod +rwx $TMPDIR/maven-status/
+chmod +rwx $TMPDIR/directory_studio_arcan_analysis/
+
+echo Check size of arcan_analysis
+
+echo ls -l $TMPDIR/directory_studio_arcan_analysis/ | wc -l
+
+ls -l $TMPDIR/directory_studio_arcan_analysis/ | wc -l
+
+echo Start program ...
+
+java -Xmx96g -jar $TMPDIR/astracker-0.9.0-jar-with-dependencies.jar -i $TMPDIR/directory_studio_arcan_analysis/ -p directory_studio -o /data/s3570282/results/directory_studio_astracker_analysis/ -pC

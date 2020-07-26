564ae7cb666044088ee3a7d9ae00a34f29207865
Thorsten Rangnau <rangnau@adference.com>
2020-07-27 00:35:14 +0200

    Aggregate smells by issue type by smell type

--- src/smell_tree_aggregation.py
+++ src/smell_tree_aggregation.py
@@ -7,11 +7,15 @@ HUBLIKE_DEPENDENCY = 'hublike_dependency'
 ISSUE_KEY = 'issue_key'
 ISSUE_TYPE = 'issue_type'
 ISSUE_PRIORITY = 'issue_priority'
+NEW_FEATURE = 'new_feature'
+IMPROVEMENT = 'improvement'
+BUG = 'bug'
+TASK = 'task'
 
 
 def extract_issue_type(row):
     if row['sub-task_parent_issue']:
-        print('I, %s, have the sub-task parent %s' % (row['smell_id'], row['sub-task_parent']))
+        # print('I, %s, have the sub-task parent %s' % (row['smell_id'], row['sub-task_parent']))
         return row['sub-task_parent']
     else:
         return row['more_likely_issue_type'] if row['more_likely_issue_type'] else row['issue_type']
@@ -60,22 +64,69 @@ def import_smell_roots_by_type(directory, name):
                 continue
             smell_id = row['smell_id']
             if cyclic_dependencies and row['root'] == 'Root':
-                print(
-                    'Row is --> %s - with smell id: \t %s \t with issue_key \t %s \t with type \t %s \t with priority \t %s' % (
-                    row['root'], row['smell_id'], row[ISSUE_KEY], row[ISSUE_TYPE], row[ISSUE_PRIORITY]))
+                # print(
+                #     'Row is --> %s - with smell id: \t %s \t with issue_key \t %s \t with type \t %s \t with priority \t %s' % (
+                #     row['root'], row['smell_id'], row[ISSUE_KEY], row[ISSUE_TYPE], row[ISSUE_PRIORITY]))
                 # print('parsing smell id \t --> \t %s' % row['smell_id'])
                 smells[CYCLIC_DEPENDENCY][smell_id] = extract_smell_information(row)
             if unstable_dependencies and row['root'] == 'Root':
-                print(
-                    'Row is --> %s - with smell id: \t %s \t with issue_key \t %s \t with type \t %s \t with priority \t %s' % (
-                        row['root'], row['smell_id'], row[ISSUE_KEY], row[ISSUE_TYPE], row[ISSUE_PRIORITY]))
+                # print(
+                #     'Row is --> %s - with smell id: \t %s \t with issue_key \t %s \t with type \t %s \t with priority \t %s' % (
+                #         row['root'], row['smell_id'], row[ISSUE_KEY], row[ISSUE_TYPE], row[ISSUE_PRIORITY]))
                 smells[UNSTABLE_DEPENDENCY][smell_id] = extract_smell_information(row)
             if hublike_dependencies and row['root'] == 'Root':
-                print(
-                    'Row is --> %s - with smell id: \t %s \t with issue_key \t %s \t with type \t %s \t with priority \t %s' % (
-                        row['root'], row['smell_id'], row[ISSUE_KEY], row[ISSUE_TYPE], row[ISSUE_PRIORITY]))
+                # print(
+                #     'Row is --> %s - with smell id: \t %s \t with issue_key \t %s \t with type \t %s \t with priority \t %s' % (
+                #         row['root'], row['smell_id'], row[ISSUE_KEY], row[ISSUE_TYPE], row[ISSUE_PRIORITY]))
                 smells[HUBLIKE_DEPENDENCY][smell_id] = extract_smell_information(row)
         return smells
+
+
+def get_smell_type_entry():
+    return {
+        NEW_FEATURE: list(),
+        IMPROVEMENT: list(),
+        BUG: list(),
+        TASK: list()
+    }
+
+
+def aggregate_smells_by_type(smells_by_type):
+    aggregated_smells = {
+        CYCLIC_DEPENDENCY: get_smell_type_entry(),
+        UNSTABLE_DEPENDENCY: get_smell_type_entry(),
+        HUBLIKE_DEPENDENCY: get_smell_type_entry()
+    }
+    for s_type, roots_by_id in smells_by_type.items():
+        for root_id, root in roots_by_id.items():
+            if s_type == CYCLIC_DEPENDENCY:
+                if root[ISSUE_TYPE] == 'New Feature':
+                    aggregated_smells[CYCLIC_DEPENDENCY][NEW_FEATURE].append(root)
+                if root[ISSUE_TYPE] == 'Improvement':
+                    aggregated_smells[CYCLIC_DEPENDENCY][IMPROVEMENT].append(root)
+                if root[ISSUE_TYPE] == 'Bug':
+                    aggregated_smells[CYCLIC_DEPENDENCY][BUG].append(root)
+                if root[ISSUE_TYPE] == 'Task':
+                    aggregated_smells[CYCLIC_DEPENDENCY][TASK].append(root)
+            if s_type == UNSTABLE_DEPENDENCY:
+                if root[ISSUE_TYPE] == 'New Feature':
+                    aggregated_smells[UNSTABLE_DEPENDENCY][NEW_FEATURE].append(root)
+                if root[ISSUE_TYPE] == 'Improvement':
+                    aggregated_smells[UNSTABLE_DEPENDENCY][IMPROVEMENT].append(root)
+                if root[ISSUE_TYPE] == 'Bug':
+                    aggregated_smells[UNSTABLE_DEPENDENCY][BUG].append(root)
+                if root[ISSUE_TYPE] == 'Task':
+                    aggregated_smells[UNSTABLE_DEPENDENCY][TASK].append(root)
+            if s_type == HUBLIKE_DEPENDENCY:
+                if root[ISSUE_TYPE] == 'New Feature':
+                    aggregated_smells[HUBLIKE_DEPENDENCY][NEW_FEATURE].append(root)
+                if root[ISSUE_TYPE] == 'Improvement':
+                    aggregated_smells[HUBLIKE_DEPENDENCY][IMPROVEMENT].append(root)
+                if root[ISSUE_TYPE] == 'Bug':
+                    aggregated_smells[HUBLIKE_DEPENDENCY][BUG].append(root)
+                if root[ISSUE_TYPE] == 'Task':
+                    aggregated_smells[HUBLIKE_DEPENDENCY][TASK].append(root)
+    return aggregated_smells
 
 
 def parse_args():
@@ -97,3 +148,18 @@ if __name__ == "__main__":
         for smell_id, smell in smells_by_id.items():
             if smell[ISSUE_TYPE] == 'Sub-task':
                 print('%s:\t %s \t is \t %s' % (smell_id, smell[ISSUE_KEY], smell[ISSUE_TYPE]))
+    aggregated_smells_by_type = aggregate_smells_by_type(smell_roots_by_type)
+    for smell_type, aggregated_by_type in aggregated_smells_by_type.items():
+        print(
+            '%s \t|\t %s \t' % (smell_type, 'value')
+        )
+        print(
+            '--------------------------------------------------------------'
+        )
+        for issue_type, counts in aggregated_by_type.items():
+            print(
+                '%s \t|\t %s \t' % (issue_type, len(counts))
+            )
+        print(
+            '--------------------------------------------------------------'
+        )
